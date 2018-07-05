4f2052a1babaf3a1fcbb76b3680cd31167f0b787
Paulo Valente <polvalente@gmail.com>
2018-07-05 17:54:45 -0300

    first commit

---
+++ lib/digit_validator.ex
@@ -0,0 +1,19 @@
+defmodule DigitValidator do
+  @moduledoc """
+  Documentation for DigitValidator.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DigitValidator.hello
+      :world
+
+  """
+  @spec validate(String.t()) :: List.t()
+  def validate(digits) do
+
+  end
+end
---
+++ test/digit_validator_test.exs
@@ -0,0 +1,8 @@
+defmodule DigitValidatorTest do
+  use ExUnit.Case
+  doctest DigitValidator
+
+  test "greets the world" do
+    assert DigitValidator.hello() == :world
+  end
+end
