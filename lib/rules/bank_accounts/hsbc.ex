8f60faf0cb463eff99fd370ea3cf1dc850bc71db
Paulo Valente <polvalente@gmail.com>
2018-07-05 23:15:11 -0300

    feature: add hsbc

---
+++ lib/rules/bank_accounts/hsbc.ex
@@ -0,0 +1,15 @@
+defmodule ChexDigits.Rules.BankAccounts.HSBC do
+  import ChexDigits.Helper
+  import Enum, only: [to_list: 1]
+
+  def check_digit(digits) do
+    digits
+    |> pad(10, :left)
+    |> checksum(
+      11,
+      [8,9,2,3,4,5,6,7,8,9],
+      replacements(%{0 => 0, 10 => 0}, %{})
+    )
+    |> List.wrap()
+  end
+end
